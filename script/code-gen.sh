3df7b07693733c68f48bd5df2651ae49ddd26a9d
Zhao KK <kikakkz@hotmail.com>
2021-11-20 01:22:44 +0000

    Correct code gen

--- script/code-gen.sh
+++ script/code-gen.sh
@@ -6,29 +6,31 @@ PROJECT_MODULE="github.com/traefik/traefik"
 MODULE_VERSION="v2"
 IMAGE_NAME="kubernetes-codegen:latest"
 
+USER="test"
+export MUID=1000
+export MGID=1000
+
 echo "Building codegen Docker image..."
-docker build --build-arg KUBE_VERSION=v0.20.2 --build-arg USER=$USER --build-arg UID=$(id -u) --build-arg GID=$(id -g) -f "./script/codegen.Dockerfile" \
+# docker build --build-arg KUBE_VERSION=v0.20.2 --build-arg USER=$USER --build-arg UID=$(id -u) --build-arg GID=$(id -g) -f "./script/codegen.Dockerfile" \
+docker build --build-arg KUBE_VERSION=v0.20.2 --build-arg USER=$USER --build-arg UID=$MUID --build-arg GID=$MGID --build-arg ALL_PROXY=$all_proxy -f "./script/codegen.Dockerfile" \
              -t "${IMAGE_NAME}" \
              "."
 
 echo "Generating Traefik clientSet code ..."
 cmd="/go/src/k8s.io/code-generator/generate-groups.sh all ${PROJECT_MODULE}/${MODULE_VERSION}/pkg/provider/kubernetes/crd/generated ${PROJECT_MODULE}/${MODULE_VERSION}/pkg/provider/kubernetes/crd traefik:v1alpha1 --go-header-file=/go/src/${PROJECT_MODULE}/script/boilerplate.go.tmpl"
 docker run --rm \
-           -v "$(pwd):/go/src/${PROJECT_MODULE}" \
            -w "/go/src/${PROJECT_MODULE}" \
            "${IMAGE_NAME}" $cmd
 
 echo "Generating DeepCopy code ..."
 cmd="deepcopy-gen --input-dirs ${PROJECT_MODULE}/${MODULE_VERSION}/pkg/config/dynamic --input-dirs ${PROJECT_MODULE}/${MODULE_VERSION}/pkg/tls --input-dirs ${PROJECT_MODULE}/${MODULE_VERSION}/pkg/types --output-package ${PROJECT_MODULE}/${MODULE_VERSION} -O zz_generated.deepcopy --go-header-file=/go/src/${PROJECT_MODULE}/script/boilerplate.go.tmpl"
 docker run --rm \
-           -v "$(pwd):/go/src/${PROJECT_MODULE}" \
            -w "/go/src/${PROJECT_MODULE}" \
            "${IMAGE_NAME}" $cmd
 
 echo "Generating the CRD definitions for the documentation ..."
 cmd="controller-gen crd:crdVersions=v1 paths=./pkg/provider/kubernetes/crd/traefik/v1alpha1/... output:dir=./docs/content/reference/dynamic-configuration/"
 docker run --rm \
-           -v "$(pwd):/go/src/${PROJECT_MODULE}" \
            -w "/go/src/${PROJECT_MODULE}" \
            "${IMAGE_NAME}" $cmd
 
--- script/codegen.Dockerfile
+++ script/codegen.Dockerfile
@@ -1,21 +1,36 @@
 FROM golang:1.17
 
-ARG USER=$USER
-ARG UID=$UID
-ARG GID=$GID
-RUN useradd -m ${USER} --uid=${UID} && echo "${USER}:" chpasswd
-USER ${UID}:${GID}
+# ARG USER=$USER
+# ARG UID=$UID
+# ARG GID=$GID
+# RUN useradd -m ${USER} --uid=${UID} && echo "${USER}:" chpasswd
+# USER ${UID}:${GID}
+
+USER root
+
+ARG ALL_PROXY
+ENV all_proxy=$ALL_PROXY
+ENV GOPROXY=https://goproxy.cn,direct
 
 ARG KUBE_VERSION
 
-RUN go get k8s.io/code-generator@$KUBE_VERSION; exit 0
-RUN go get k8s.io/apimachinery@$KUBE_VERSION; exit 0
-RUN go get k8s.io/code-generator/cmd/deepcopy-gen@$KUBE_VERSION; exit 0
-RUN go get sigs.k8s.io/controller-tools/cmd/controller-gen@v0.6.2; exit 0
+RUN echo $all_proxy
+RUN all_proxy=$ALL_PROXY go get k8s.io/code-generator@$KUBE_VERSION; exit 0
+RUN all_proxy=$ALL_PROXY go get k8s.io/apimachinery@$KUBE_VERSION; exit 0
+RUN all_proxy=$ALL_PROXY go get k8s.io/code-generator/cmd/deepcopy-gen@$KUBE_VERSION; exit 0
+RUN all_proxy=$ALL_PROXY go get sigs.k8s.io/controller-tools/cmd/controller-gen@v0.6.2; exit 0
+
+# COPY go.mod .
+# COPY go.sum .
+# RUN GO111MODULE=on GOPROXY=https://goproxy.cn,direct go mod download
+COPY . /go/src/github.com/traefik/traefik
+COPY . /go/src/github.com/traefik/traefik/v2
+RUN ls /go/src/github.com/traefik/traefik/script
 
 RUN mkdir -p $GOPATH/src/k8s.io/{code-generator,apimachinery}
 RUN cp -R $GOPATH/pkg/mod/k8s.io/code-generator@$KUBE_VERSION $GOPATH/src/k8s.io/code-generator
 RUN cp -R $GOPATH/pkg/mod/k8s.io/apimachinery@$KUBE_VERSION $GOPATH/src/k8s.io/apimachinery
 RUN chmod +x $GOPATH/src/k8s.io/code-generator/generate-groups.sh
 
+RUN ls /go/src/github.com/traefik/traefik/script
 WORKDIR $GOPATH/src/k8s.io/code-generator
