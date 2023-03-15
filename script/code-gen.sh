e68a7d31cb678afdba20853d4399f57fcdda723e
Zhao KK <kikakkz@hotmail.com>
2023-03-15 06:31:52 +0000

    Do not mount pwd, we'll run in docker in docker

--- script/code-gen.sh
+++ script/code-gen.sh
@@ -25,21 +25,21 @@ docker build --build-arg KUBE_VERSION=v0.20.2 \
 echo "Generating Traefik clientSet code ..."
 cmd="/go/src/k8s.io/code-generator/generate-groups.sh all ${PROJECT_MODULE}/${MODULE_VERSION}/pkg/provider/kubernetes/crd/generated ${PROJECT_MODULE}/${MODULE_VERSION}/pkg/provider/kubernetes/crd traefik:v1alpha1 --go-header-file=/go/src/${PROJECT_MODULE}/script/boilerplate.go.tmpl"
 docker run --rm \
-           -v "${CURRENT_DIR}:/go/src/${PROJECT_MODULE}" \
+           # -v "${CURRENT_DIR}:/go/src/${PROJECT_MODULE}" \
            -w "/go/src/${PROJECT_MODULE}" \
            "${IMAGE_NAME}" ${cmd}
 
 echo "Generating DeepCopy code ..."
 cmd="deepcopy-gen --input-dirs ${PROJECT_MODULE}/${MODULE_VERSION}/pkg/config/dynamic --input-dirs ${PROJECT_MODULE}/${MODULE_VERSION}/pkg/tls --input-dirs ${PROJECT_MODULE}/${MODULE_VERSION}/pkg/types --output-package ${PROJECT_MODULE}/${MODULE_VERSION} -O zz_generated.deepcopy --go-header-file=/go/src/${PROJECT_MODULE}/script/boilerplate.go.tmpl"
 docker run --rm \
-           -v "${CURRENT_DIR}:/go/src/${PROJECT_MODULE}" \
+           # -v "${CURRENT_DIR}:/go/src/${PROJECT_MODULE}" \
            -w "/go/src/${PROJECT_MODULE}" \
            "${IMAGE_NAME}" ${cmd}
 
 echo "Generating the CRD definitions for the documentation ..."
 cmd="controller-gen crd:crdVersions=v1 paths=./pkg/provider/kubernetes/crd/traefik/v1alpha1/... output:dir=./docs/content/reference/dynamic-configuration/"
 docker run --rm \
-           -v "${CURRENT_DIR}:/go/src/${PROJECT_MODULE}" \
+           # -v "${CURRENT_DIR}:/go/src/${PROJECT_MODULE}" \
            -w "/go/src/${PROJECT_MODULE}" \
            "${IMAGE_NAME}" ${cmd}
 
